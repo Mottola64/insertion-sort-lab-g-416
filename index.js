fb9cac470b6ea49de344d64ef5dcd77b1d1d1ccb
Michael Mottola <mottola64@gmail.com>
2020-09-23 21:16:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 function findMinAndRemove(array){
-  let smallest = Math.min()
-return array.smallest.pop()
+  let currentMin = array[0]
+  let minIndex = 0
+  for(let i = 0; i < array.length; i++){
+    if(array[i] < currentMin){
+      currentMin = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1);
+  return currentMin;
 }
-
 function insertionSort(array){
 
 }
